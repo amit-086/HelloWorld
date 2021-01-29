d00801aac61e8b15478a5d85a3fd9e3b7536590d
amit-086 <55597266+amit-086@users.noreply.github.com>
2021-01-29 14:28:49 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@
 {
     public static void main(String[] args) {
       System.out.println("Hello World!! ");
-      System.out.println("testing webhook!! ");
+      System.out.println("testing webhook!");
     }
 }
